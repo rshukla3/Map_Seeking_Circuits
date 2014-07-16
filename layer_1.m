19bf7018863bea844419cbbcbf3bac87593afab7
Rohit Shukla <rshukla3@wisc.edu>
2014-07-16 18:52:45 -0500

    Removed the parfor loop from layer_1
    Removed the parfor loop for layer_1, since the variables in the function
    xTranslate_sum and yTranslate_sum were loop dependent.

--- layer_1.m
+++ layer_1.m
@@ -4,7 +4,7 @@ function [ xyTranslated_Img ] = layer_1( Test_Img,  translationCount, xTranslate
 
 xTranslate_sum = Test_Img;
 
-parfor i = 1:translationCount
+for i = 1:translationCount
     xT = (i*xTranslateQuantity);
     
     xTranslate_sum = xTranslate_sum + translate_img(Test_Img, -xT, 0);
@@ -15,7 +15,7 @@ end
 yTranslate_sum = xTranslate_sum;
 
 superposition_Img_xTranslate = xTranslate_sum;
-parfor i = 1:translationCount
+for i = 1:translationCount
     yT = (i*yTranslateQuantity);
     
     yTranslate_sum = yTranslate_sum + translate_img(superposition_Img_xTranslate, 0, -yT);
