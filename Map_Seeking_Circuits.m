deac4d4cd17ba4539e31c4ed1984ef057746b49d
Rohit Shukla <rshukla3@wisc.edu>
2014-07-15 21:16:26 -0500

    Implemented the translate layer of MSC
    Currently implemented the translate layer of Map Seeking Circuits. Still
    need to figure out how to implement gi and qi in this algorithmic
    circuit.

--- Map_Seeking_Circuits.m
+++ Map_Seeking_Circuits.m
@@ -20,13 +20,47 @@ clear Read_Test_Img;
 
 %% Perform translation on the test image.
 
-J = translate_img(Memory_Img, -150, 150);
+% J = translate_img(Memory_Img, -150, 150);
+% 
+% figure(1);
+% imshow(Memory_Img);
+% 
+% figure(2);
+% imshow(Test_Img);
+% 
+% figure(3);
+% imshow(J);
+
+%% First perform x-translation
+
+[M1, M2] = size(Memory_Img);
+
+%xTranslate_sum = zeros(M1, M2);
+
+xTranslate_sum = Test_Img;
+
+translate = 10;
+for i = 1:15
+    xT = (i*80)+translate;
+    
+    xTranslate_sum = xTranslate_sum + translate_img(Test_Img, -xT, 0);
+    
+    xTranslate_sum = xTranslate_sum + translate_img(Test_Img, xT, 0);
+end
 
 figure(1);
-imshow(Memory_Img);
+imshow(xTranslate_sum);
+
+yTranslate_sum = xTranslate_sum;
+
+superposition_Img_xTranslate = xTranslate_sum;
+for i = 1:10
+    yT = (i*100)+translate;
+    
+    yTranslate_sum = yTranslate_sum + translate_img(superposition_Img_xTranslate, 0, -yT);
+    
+    yTranslate_sum = yTranslate_sum + translate_img(superposition_Img_xTranslate, 0, yT);
+end
 
 figure(2);
-imshow(Test_Img);
-
-figure(3);
-imshow(J);
+imshow(yTranslate_sum);
