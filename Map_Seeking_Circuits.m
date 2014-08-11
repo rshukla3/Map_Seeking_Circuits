a5777feb34f299c4926a496029c6be9bad04573e
Rohit Shukla <rshukla3@wisc.edu>
2014-08-11 17:18:01 -0500

    Trying to recreate the image translation and rotation experiment
    Trying to recreate the image translation and rotation experiment. Not
    complete and still half done.

--- Map_Seeking_Circuits.m
+++ Map_Seeking_Circuits.m
@@ -7,28 +7,28 @@ clc;
 % 1. Set the parameters for image translation.
 
 % By how much the image should be translated on x-axis.
-xTranslateQuantity = 90;
+xTranslateQuantity = 30;
 
 % By how much the image should be translated on y-axis.
-yTranslateQuantity = 90;
+yTranslateQuantity = 30;
 
 % Number of times this x-translation that has to be applied.
-xTranslationCount = 4;
+xTranslationCount = 15;
 
 % Number of times this x-translation that has to be applied.
-yTranslationCount = 4;
+yTranslationCount = 15;
 
 % 2. Set the parameters for image rotation.
 
 % This term defines the number of times input image should be rotated
-rotationCount = 2;
+rotationCount = 6;
 
 % The precision by which image should be rotated.
-rotationQuantity = 45;
+rotationQuantity = 15;
 
 % 3. Number of iterations for which Map_Seeking Circuit architecture will
 % run.
-iterationCount = 15;
+iterationCount = 20;
 
 % 4. Set the value of constants k, for multiplication with g.
 
@@ -47,36 +47,36 @@ gThresh = 0.3;
 scaleCount = 3;
 
 % Factor by which each time an image is scaled.
-scaleFactor = 0.1;
+scaleFactor = 0.2;
 
 % 7. Number of elements that are present in the memory unit.
 
-memory_units = 2;
+memory_units = 1;
 
-%% Read the image that is to be stored in memory.
+%% Read the image that is to be stored in memory
 
 Read_Memory_Img = imread('Memory_Img_small_Rectangle_1.jpg');
-Read_Memory_Img_1 = imread('Memory_Img_small_Circle.jpg');
+%Read_Memory_Img_1 = imread('Memory_Img_small_Circle.jpg');
 
 %% Read the test image that is to be recognized against the image stored in memory.
 
-%Read_Test_Img = imread('Test_Img_Rotated_Rectangle.jpg');
-Read_Test_Img = imread('Test_Img_Circle.jpg');
+Read_Test_Img = imread('Test_Img_Rotated_Rectangle.jpg');
+%Read_Test_Img = imread('Test_Img_Circle.jpg');
 %Read_Test_Img = Read_Memory_Img; 
 %Read_Test_Img = translate_img(Read_Test_Img, -90, 90);
 
 %% Convert he two images to grayscale.
 
 Memory_Img_gray = rgb2gray(Read_Memory_Img);
-Memory_Img_gray_1 = rgb2gray(Read_Memory_Img_1);
+%Memory_Img_gray_1 = rgb2gray(Read_Memory_Img_1);
 Test_Img_gray = rgb2gray(Read_Test_Img);
 
 Level_Memory_Img = graythresh(Memory_Img_gray);
-Level_Memory_Img_1 = graythresh(Memory_Img_gray_1);
+%Level_Memory_Img_1 = graythresh(Memory_Img_gray_1);
 Level_Test_Img = graythresh(Test_Img_gray);
 
 Memory_Img = im2bw(Memory_Img_gray, Level_Memory_Img);
-Memory_Img_1 = im2bw(Memory_Img_gray_1, Level_Memory_Img_1);
+%Memory_Img_1 = im2bw(Memory_Img_gray_1, Level_Memory_Img_1);
 Test_Img = im2bw(Test_Img_gray, Level_Test_Img);
 
 %Test_Img_Rotated = imrotate(Memory_Img, 45, 'nearest', 'crop');
@@ -110,15 +110,15 @@ for i = 1:iterationCount
 %% Set the value of backward path 
 
     % First initialize the value of backward path in the last layer.
-    b5 = g_mem(1)*Memory_Img + g_mem(2)*Memory_Img_1;
+    b5 = g_mem(1)*Memory_Img; % + g_mem(2)*Memory_Img_1;
 
     % Assign rest of the values for the backward path.
     
      % Perform inverse scaling on the backward layer.
-    [b4, Tf_tmp, q_layer4_scaling] = layer_4(b5, scaleCount, scaleFactor, g_layer4, 'backward');
+    %[b4, Tf_tmp, q_layer4_scaling] = layer_4(b5, scaleCount, scaleFactor, g_layer4, 'backward');
 
     % Perform inverse rotation on the backward layer.
-    b3 = layer_3(b4, rotationCount, rotationQuantity, g_layer3, 'backward');
+    b3 = layer_3(b5, rotationCount, rotationQuantity, g_layer3, 'backward');
 
     % Perform inverse translation on the superimposed image along y-axis.
     b2 = layer_2(b3, yTranslationCount, yTranslateQuantity, g_layer2, 'backward');
@@ -134,7 +134,7 @@ for i = 1:iterationCount
 
     q_layer3(1:2*rotationCount+1) = single(zeros(1,2*rotationCount+1));
     
-    q_layer4(1:2*scaleCount+1) = single(zeros(1,2*scaleCount+1));
+    %q_layer4(1:2*scaleCount+1) = single(zeros(1,2*scaleCount+1));
     
 
 %% Perform transformation on the image.
@@ -155,19 +155,19 @@ for i = 1:iterationCount
     [f3, Tf2] = layer_3(f2, rotationCount, rotationQuantity, g_layer3, 'forward');
     
     %Calculate the value of q_layer3.
-    q_layer3(1:2*rotationCount+1) = dotproduct(Tf2, b4);
+    q_layer3(1:2*rotationCount+1) = dotproduct(Tf2, b5);
     
      % Scale the rotated image.
-    [f4, Tf3] = layer_4(f3, scaleCount, scaleFactor, g_layer4, 'forward');
+    %[f4, Tf3] = layer_4(f3, scaleCount, scaleFactor, g_layer4, 'forward');
     
     %Calculate the value of q_layer4, or the layer that performs scaling operation.
-    q_layer4(1:2*scaleCount+1) = dotproduct(Tf3, b5);
+    %q_layer4(1:2*scaleCount+1) = dotproduct(Tf3, b5);
     
-    q_layer4(1:2*scaleCount+1) = q_layer4(1:2*scaleCount+1).*q_layer4_scaling(1:2*scaleCount+1);
+    %q_layer4(1:2*scaleCount+1) = q_layer4(1:2*scaleCount+1).*q_layer4_scaling(1:2*scaleCount+1);
     
     %Calculate the q values for memory layer.
-    q_layer_mem = [dot(single(Memory_Img(:)), single(f4(:))) dot(single(Memory_Img_1(:)), single(f4(:)))*(1.0)];
-    
+    % q_layer_mem = [dot(single(Memory_Img(:)), single(f4(:))) dot(single(Memory_Img_1(:)), single(f4(:)))*(1.0)];
+    q_layer_mem = dot(single(Memory_Img(:)), single(f3(:)));
 %% Select the value of g_layers based on the q values that have been computed
 
     g_layer1 = g_layer1 - k_xTranslation*( 1-( q_layer1./max(q_layer1) ) );
@@ -176,14 +176,14 @@ for i = 1:iterationCount
 
     g_layer3 = g_layer3 - k_rotation*( 1-( q_layer3./max(q_layer3) ) );
     
-    g_layer4 = g_layer4 - k_scaling*( 1-( q_layer4./max(q_layer4) ) );
+    %g_layer4 = g_layer4 - k_scaling*( 1-( q_layer4./max(q_layer4) ) );
     
     g_mem = g_mem - k_mem*( 1-( q_layer_mem./max(q_layer_mem) ) );
     
     g_layer1 = g_threshold(g_layer1, gThresh);
     g_layer2 = g_threshold(g_layer2, gThresh);
     g_layer3 = g_threshold(g_layer3, gThresh);
-    g_layer4 = g_threshold(g_layer4, gThresh);
+    %g_layer4 = g_threshold(g_layer4, gThresh);
     g_mem = g_threshold(g_mem, gThresh);
 end
 
@@ -196,14 +196,14 @@ imshow(f2);
 figure(3);
 imshow(f3);
 
-figure(4);
-imshow(f4);
+%figure(4);
+%imshow(f4);
 
 figure(5);
 imshow(b5);
 
-figure(6);
-imshow(b4);
+%figure(6);
+%imshow(b4);
 
 figure(7);
 imshow(b3);
