df3407d1dffd9965fc168c2d9070070117766e56
Rohit Shukla <rshukla3@wisc.edu>
2014-09-09 19:01:49 -0500

    New

--- Learning_Orthogonal_Maps/Map_Seeking_Circuits_Real_Image.m
+++ Learning_Orthogonal_Maps/Map_Seeking_Circuits_Real_Image.m
@@ -121,13 +121,7 @@ if(isempty(Memory_Img))
     imwrite(Memory_Img(:, :, 1), 'Memory_Images.tif');    
 end
 Memory_Img(:, :, 1) = single(Test_Img);
-g_mem(1:memory_units) = single(ones(memory_units,1));
 
-%% Initialize the value of g to all ones for the three layers.
-
-g_layer1(1:2*xTranslationCount+1) = single(ones(1,2*xTranslationCount+1));
-g_layer2(1:2*yTranslationCount+1) = single(ones(1,2*yTranslationCount+1));
-g_layer3(1:2*rotationCount+1) = single(ones(1,2*rotationCount+1));
 
 %% Number of images to be learned by MSC.
 
@@ -145,6 +139,108 @@ for i = 1:Movie_Image_Count_Vertical
        f(:,:,1) = Test_Img;       
 end
 
+
+%% Read the diagonally translated images for learning in MSC.
+fname = 'Diagonal_Movie_Matrix.mat';
+if exist(fname, 'file') == 2
+    load('Diagonal_Movie_Matrix.mat', 'Diagonal_Movie_Matrix');    
+else
+    fprintf('The selected .mat file does not exist\n');    
+    exit(0);
+end
+
+Test_Img = single(Diagonal_Movie_Matrix(:,:,1));
+
+g_mem(1:memory_units) = single(ones(memory_units,1));
+
+%% Initialize the value of g to all ones for the three layers.
+
+g_layer1(1:(Movie_Image_Count_Horizontal*Movie_Image_Count_Vertical)) = single(ones(1,(Movie_Image_Count_Horizontal*Movie_Image_Count_Vertical)));
+g_layer3(1:2*rotationCount+1) = single(ones(1,2*rotationCount+1));
+
+for i = 1:iterationCount
+    
+% Set the value of backward path 
+    b(:,:,layerCount) = layer_memory(g_mem, Memory_Img, memory_units);
+    
+       
+    if(Transformation >= 1)        
+        % Perform inverse translation on the superimposed image along x-axis.
+        b(:,:,1) = layer_1(b(:,:,2), xTranslationCount, xTranslateQuantity, g_layer1, 'backward');
+    end    
+    
+    if(Transformation >= 1)
+        q_xTranslation(1:2*xTranslationCount+1) = single(zeros(1,2*xTranslationCount+1));
+        % Translate the image along x-axis.
+        [f(:,:,2), Tf0] = layer_1(Test_Img, xTranslationCount, xTranslateQuantity, g_layer1, 'forward');    
+        %Calculate the value of q_xTranslation.
+        q_xTranslation(1:2*xTranslationCount+1) = dotproduct(Tf0, b(:,:,2));  
+    end   
+    
+    f(:,:,1) = (Test_Img);
+    q_Top_Layer = dotproduct(f(:,:,1), b(:,:,1));
+    
+    q_layer_mem(1:memory_units) = single(zeros(1,memory_units));
+    %Calculate the value of q_layer_mem.    
+    q_layer_mem(1:memory_units) = dotproduct(Memory_Img, f(:,:,layerCount));       
+    
+    %q_layer_mem = dot(single(Memory_Img(:)), single(f3(:)));
+    
+    fprintf('The value of iterationCount is: %d i is: %d\n', iterationCount, i); 
+    
+
+    
+% Set the value of q to all zeros for the three layers.    
+    if(isempty(q_mem))
+        q_Top_Layer = dotproduct(Memory_Img(:, :, 1), Memory_Img(:, :, 1));
+        q_mem(1) = q_Top_Layer;
+        q_units = 2;
+        
+        q_Top_Layer = dotproduct(Memory_Img(:, :, 2), Memory_Img(:, :, 2));
+        q_mem(2) = q_Top_Layer;
+        dlmwrite('q_mem.txt', q_mem, '\t');
+    else
+        if(q_Top_Layer<0.4*q_mem(1))
+            if(layerCount < 4)
+                Transformation = Transformation+1;            
+                layerCount = layerCount+1;
+                b(:,:,layerCount) = b(:,:,layerCount-1);            
+            end
+        else
+            if(Transformation >= 1)
+                g_layer1 = g_layer1 - k_xTranslation*( 1-( q_xTranslation./max(q_xTranslation) ) );
+                g_layer1 = g_threshold(g_layer1, gThresh);
+                if(nnz(g_layer1)==1)
+                    xTranslation_converged = 1;
+                end
+            end    
+                
+            if(Transformation >= 2)
+                g_layer2 = g_layer2 - k_yTranslation*( 1-( q_yTranslation./max(q_yTranslation) ) );
+                g_layer2 = g_threshold(g_layer2, gThresh);
+                if(nnz(g_layer2)==1)
+                    yTranslation_converged = 1;
+                end
+            end 
+            
+            if(Transformation >= 3)
+                g_layer3 = g_layer3 - k_rotation*( 1-( q_rotation./max(q_rotation) ) );
+                g_layer3 = g_threshold(g_layer3, gThresh);
+                if(nnz(g_layer3)==1)
+                    rotation_converged = 1;
+                end
+            end
+            
+            g_mem = g_mem - k_mem*( 1-( q_layer_mem./max(q_layer_mem) ) );
+            g_mem = g_threshold(g_mem, gThresh);
+            
+            if(nnz(g_mem)==1)
+                memory_converged = 1;
+            end
+        end
+    end    
+    
+end
 
 %b(:,:,1) = Transformation_Matrix(:,:,1)+Memory_Img(:,:,1);
 figure(3);
---
+++ Learning_Orthogonal_Maps/layer_1_newTransformation.m
@@ -0,0 +1,45 @@
+function [ xTranslated_Img, Transformation_Vector ] = layer_1_newTransformation( Test_Img_Input,  translationCount, xTranslateQuantity, g, path)
+% layer_1: This is the first layer of map seeking circuits where image 
+% translation is performed on x and y axes.
+
+Test_Img = single(Test_Img_Input);
+
+% fprintf('Test image after single conversion\n');
+% display(Test_Img);
+
+[m,n] = size(Test_Img);
+
+Transformation_Vector = single(zeros(m,n,2*translationCount+1));
+
+% Value of f0 is 0. So the vector T(f0) should be a zero vector.
+%Transformation_Vector_Zero = Transformation_Vector;
+
+xTranslate_sum = g(translationCount+1)*Test_Img;
+Transformation_Vector(1:m,1:n,translationCount+1) = xTranslate_sum;
+
+for i = 1:translationCount
+    
+        xT = (i*xTranslateQuantity);
+    
+        if((g(i) ~=0)&&(strcmpi(path, 'forward')))            
+            Transformation_Vector(1:m,1:n,i) = (g(i)*translate_img(Test_Img, xT, 0));
+            xTranslate_sum = xTranslate_sum + Transformation_Vector(1:m,1:n,i);
+        end
+        
+        if((g(i+translationCount+1) ~=0)&&(strcmpi(path, 'forward')))
+             Transformation_Vector(1:m,1:n,(i+translationCount+1)) = (g(i+translationCount+1)*translate_img(Test_Img, -xT, 0));
+             xTranslate_sum = xTranslate_sum + Transformation_Vector(1:m,1:n,(i+translationCount+1));
+        end
+        
+        if((g(i) ~=0)&&(strcmpi(path, 'backward')))
+            xTranslate_sum = xTranslate_sum + (g(i)*translate_img(Test_Img, -xT, 0));
+        end
+        
+        if((g(i+translationCount+1) ~=0)&&(strcmpi(path, 'backward')))
+            xTranslate_sum = xTranslate_sum + (g(i+translationCount+1)*translate_img(Test_Img, xT, 0));
+        end
+end
+
+xTranslated_Img = single(xTranslate_sum); 
+end
+
