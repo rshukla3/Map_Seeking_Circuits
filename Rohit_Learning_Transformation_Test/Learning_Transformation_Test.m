0abcc98f3173dc7a960abe887cedcd3043664a1a
Rohit Shukla <rshukla3@wisc.edu>
2014-09-11 12:30:38 -0500

    working learned transformations

---
+++ Rohit_Learning_Transformation_Test/Learning_Transformation_Test.m
@@ -0,0 +1,104 @@
+clc;
+close all;
+clear all;
+
+% 2. Read the already stored images from tif image file.
+fname = 'Memory_Images.tif';
+if exist(fname, 'file') == 2
+    info = imfinfo(fname);
+    memory_units = numel(info);
+    for k = 1:memory_units
+        Memory_Img(:,:,k) = single(logical(imread(fname, k, 'Info', info)));
+    end
+else
+    Memory_Img = [];
+end
+
+
+%% Read the test image and the image that is to be stored in memory.
+
+% Read the test image and the image that is to be stored in memory and
+% later do preprocessing on them.
+
+[Test_Img] = imagePreProcessing('pepper_2.jpg');
+Test_Img = single(imrotate(Test_Img, 180, 'nearest', 'crop'));
+Test_Img = translate_img(Test_Img, 100, -100);
+%% Transform the image matrix to single dimension.
+% Transform the image matrix to single dimension with only the value (or 
+% coordinates of the image) in it.
+
+[m,n,memory_count] = size(Memory_Img);
+
+Memory_Img_tmp = Memory_Img(:,:,1);
+
+index = 1;
+for i = 1:m
+    for j = 1:n
+        if(Memory_Img_tmp(i,j) == 1)
+            Coordinate(index,1) = i;
+            Coordinate(index,2) = j;
+            Coordinate(index,3) = 1;
+            index = index + 1;
+        end
+    end
+end
+
+[tm,tn] = size(Test_Img);
+
+index = 1;
+for i = tm:-1:1
+    for j = tm:-1:1
+        if(Test_Img(i,j) == 1)
+            Coordinate_Test(index,1) = i;
+            Coordinate_Test(index,2) = j;
+            Coordinate_Test(index,3) = 1;
+            index = index + 1;
+        end
+    end
+end
+figure(1);
+imshow(Memory_Img_tmp);
+
+figure(2);
+imshow(Test_Img);
+
+M = Coordinate\Coordinate_Test;
+
+[m,n] = size(M);
+
+for i = 1:m
+    for j = 1:n
+        if(abs(M(i,j)) < 0.001)
+            M(i,j) = 0;
+        end
+        if(abs(M(i,j)-1) < 0.001)
+            M(i,j) = 1;
+        end
+    end
+end
+
+
+[Test_Img] = imagePreProcessing('sailboat_2.jpg');
+figure(3);
+imshow(Test_Img);
+[tm,tn] = size(Test_Img);
+
+index = 1;
+
+
+New_Img = zeros(tm,tn);
+for i = 1:tm
+    for j = 1:tn
+        if(Test_Img(i,j) == 1)
+            P = [i j 1]*M;
+            Coordinate_Test(index,1) = P(1);
+            Coordinate_Test(index,2) = P(2);
+            Coordinate_Test(index,3) = P(3);
+            New_Img(fix(P(1)), fix(P(2))) = 1;
+            index = index + 1;
+        end
+    end
+end
+
+figure(4);
+imshow(New_Img);
---
+++ Rohit_Learning_Transformation_Test/imagePreProcessing.m
@@ -0,0 +1,32 @@
+function [ Test_Img ] = imagePreProcessing(filename)
+%Does the preprocessing on input images. PreProcessing involves performing
+%filtering on the image and later doing edge detection on it.
+
+Read_Test_Img = imread(filename);
+
+Test_Img_gray = rgb2gray(Read_Test_Img);
+
+[Im, In] = size(Test_Img_gray);
+
+[~, Thresh] = edge(Test_Img_gray, 'prewitt');
+Test_Img_BW = edge(Test_Img_gray, 'prewitt', Thresh*0.5);
+se90 = strel('line', 3, 90);
+se0 = strel('line', 3, 0);
+Test_Img_dilate = imdilate(Test_Img_BW, [se90 se0]);
+Test_Img_Fill = imfill(Test_Img_dilate, 'holes');
+seD = strel('diamond',1);
+Test_Img_Final = imerode(Test_Img_Fill,seD);
+Test_Img_Erode = imerode(Test_Img_Final,seD);
+BWoutline = edge(Test_Img_Erode);
+Test_Img = logical(zeros(Im, In));
+Test_Img(BWoutline) = 1;
+Test_Img = single(Test_Img);
+%Test_Img = single(imrotate(Test_Img, 45, 'nearest', 'crop'));
+%Test_Img = translate_img(Test_Img, 100, -150);
+%Test_Img = Test_Img.*255;
+
+%figure(2);
+%imshow(Test_Img);
+
+end
+
