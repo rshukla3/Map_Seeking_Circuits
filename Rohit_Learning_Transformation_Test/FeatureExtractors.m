37f742b762d3b8bdeb9c8d615ca2b7c0c0be1776
Rohit Shukla <rshukla3@wisc.edu>
2014-10-27 17:25:44 -0500

    Computer vision toolbox seems is not there with the university

--- Rohit_Learning_Transformation_Test/FeatureExtractors.m
+++ Rohit_Learning_Transformation_Test/FeatureExtractors.m
@@ -1,4 +1,5 @@
-function [ points ] = FeatureExtractors( Memory_Img )
-points = detectFASTFeatures(double(Memory_Img));
+function [ corners ] = FeatureExtractors( Memory_Img )
+Read_Test_Img = imread('pepper_2.jpg');
+corners = detectHarrisFeatures(Read_Test_Img);
 end
 
