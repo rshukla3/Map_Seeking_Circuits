b1306b51d5d4d34bbfed81b19832dbbfef290931
Rohit Shukla <rshukla3@wisc.edu>
2014-11-05 19:24:59 -0600

    Files modified during presentation to Prof. Mikko

--- Rohit_Learning_Transformation_Test/Learn_Different_Transformations.m
+++ Rohit_Learning_Transformation_Test/Learn_Different_Transformations.m
@@ -129,8 +129,8 @@ end
 % generated affine transformations.
 % Test_Img = Img_PointsOfInterest;
 % Test_Img = translate_img(Img_PointsOfInterest, 100, 100);
-% Test_Img = single(imrotate(Img_PointsOfInterest, 180, 'nearest', 'crop'));
-Test_Img = scaleImg(Img_PointsOfInterest, 1.6, 1.6);
+Test_Img = single(imrotate(Img_PointsOfInterest, -90, 'nearest', 'crop'));
+% Test_Img = scaleImg(Img_PointsOfInterest, 1.6, 1.6);
 %% Degenerate layer that just does identity multiplication.
 
 % We will start off with a degerate layer that just performs identity
